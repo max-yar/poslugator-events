5816a961de12c6036089213f2d25463cda897663
YarAllex <32192351+YarAllex@users.noreply.github.com>
2026-05-30 22:28:09 +0300

    fix: Fix jackson sealed serialisation

--- src/main/java/com/poslugator/events/account/AccountEvent.java
+++ src/main/java/com/poslugator/events/account/AccountEvent.java
@@ -1,8 +1,20 @@
 package com.poslugator.events.account;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.time.Instant;
 import java.util.UUID;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "eventType")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = AccountCreatedEvent.class, name = "ACCOUNT_CREATED"),
+    @JsonSubTypes.Type(value = VerifyEmailEvent.class, name = "VERIFY_EMAIL"),
+    @JsonSubTypes.Type(value = ChangeEmailEvent.class, name = "CHANGE_EMAIL"),
+    @JsonSubTypes.Type(value = ForgotPasswordEvent.class, name = "FORGOT_PASSWORD"),
+    @JsonSubTypes.Type(value = AddPasswordEvent.class, name = "ADD_PASSWORD"),
+    @JsonSubTypes.Type(value = ChangePasswordEvent.class, name = "CHANGE_PASSWORD"),
+    @JsonSubTypes.Type(value = DeleteAccountEvent.class, name = "DELETE_ACCOUNT"),
+})
 public sealed interface AccountEvent
         permits AccountCreatedEvent,
                 VerifyEmailEvent,
